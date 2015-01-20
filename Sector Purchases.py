f1bc93cfc58f0d7e4324ee906fef984b8b1ec541
Bilal Arif <bilal764@hotmail.com>
2015-01-20 12:36:20 +0000

    sector purchases

---
+++ Sector Purchases.py
@@ -0,0 +1,23 @@
+import urllib2
+import json
+
+url = "http://dev.c0l.in:8888/"
+
+response = urllib2.urlopen(url).read()
+data = json.loads(response.decode('utf8'))
+
+# Specific Sectors Purchases:
+purchases = raw_input("which Sector purchases would you like to view?: ")
+found = False
+for record in data:
+    if str(record["sector"]) == purchases:
+        print record["company"]["name"]
+        print record["company"]["purchases"]
+        
+        
+
+
+
+
+
+
